bd7375ae2813057a2425ecc2b94b6da2d5834b1a
decentstates <ds@decentstates.com>
2025-12-01 18:53:07 +0700

    Log to stderr

--- prspct/src/prspct/core.clj
+++ prspct/src/prspct/core.clj
@@ -6,8 +6,11 @@
  (:gen-class))
 
 (defn -main [& args]
-  (if (= (apply cli/-main args)
-         :error-exit)
-    (java.lang.System/exit 1)
-    (java.lang.System/exit 0)))
+  (tel/add-handler! ::console (tel/handler:console {:stream *err*}))
+  (tel/remove-handler! :default/console)
+  (let [ret (apply cli/-main args)]
+    (tel/stop-handlers!)
+    (if (= :error-exit ret)
+      (java.lang.System/exit 1)
+      (java.lang.System/exit 0))))
 
